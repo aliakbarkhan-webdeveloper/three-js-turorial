d2e29bd4f70366109a639e7662cffe093129d3f5
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-23 19:09:59 +0500

    commit

--- tutorial/index.js
+++ tutorial/index.js
@@ -8,22 +8,23 @@ const camera = new THREE.PerspectiveCamera(
   10
 );
 const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: "red"});
+const material = new THREE.MeshBasicMaterial({ color: "red" });
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
-// camera.position.z = 5;
-cube.rotation.x=3;
-const canvas=document.querySelector("#draw")
-const renderer = new THREE.WebGLRenderer({canvas:canvas});
+camera.position.z = 6;
+cube.position.x = 3;
+cube.scale.y = 1.2;
+const canvas = document.querySelector("#draw");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-
+const clock = new THREE.Clock();
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  cube.rotation.y = clock.getElapsedTime() * 3;
   renderer.render(scene, camera);
 }
 
